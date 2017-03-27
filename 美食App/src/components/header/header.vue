29e4b12fc82edb735062d7e579b245cb616e6f07
晓&明 <864136911@qq.com>
2017-03-27 23:08:38 +0800

    header css style

--- 美食App/src/components/header/header.vue
+++ 美食App/src/components/header/header.vue
@@ -13,9 +13,13 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-      	  <span class="icon"></span>
+      	  <span class="icon" v-bind:class="classMap[seller.supports[0].type]"></span>
       	  <span class="text">{{seller.supports[0].description}}</span>
         </div>
+      </div>
+      <div v-if="seller.supports" class="support-count">
+      	<span class="count">{{seller.supports.length}}个</span>
+      	<i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
     <div class="bulletin-wrapper">
@@ -30,6 +34,9 @@
       seller: {
         type: Object
       }
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
   };
 </script>
@@ -39,12 +46,15 @@
 
   .header
     color: #fff
-    background: #000
+    background: #010
     .content-wrapper
       padding: 24px 12px 18px 24px
-      font-size:0
+      font-size: 0
       .avatar
         display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
       .content
         display: inline-block
         margin-left: 16px
@@ -64,5 +74,46 @@
             font-size: 16px
             line-height: 18px
             font-weight: bold
-
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+        .support
+          .icon
+            display: inline-block
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            vertical-align: top    
+            line-height: 12px
+            font-size: 10px
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, 0.2)
+        text-align: center
+        .count
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          line-height: 24px
+          font-size: 10px
+          margin-left: 2px
 </style>
